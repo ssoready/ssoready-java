ec75f0eef62d53077f6537b7d056bcea73814371
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-04 19:31:41 +0000

    SDK regeneration

--- src/main/java/com/ssoready/api/core/ClientOptions.java
+++ src/main/java/com/ssoready/api/core/ClientOptions.java
@@ -30,7 +30,7 @@ public final class ClientOptions {
             {
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.ssoready.fern:api-sdk");
-                put("X-Fern-SDK-Version", "0.1.3");
+                put("X-Fern-SDK-Version", "0.1.5");
             }
         });
         this.headerSuppliers = headerSuppliers;
